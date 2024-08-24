b298f7c318ab7e9e6c90d981437d26f3ea96e85a
Viktoria Semichaevskaya <viktoria.vodohlebova4579700@gmail.com>
2024-08-24 20:56:12 +0500

    first commit

---
+++ dist/index.c36f364e.js
@@ -0,0 +1,4 @@
+console.log("hello");
+require();
+
+//# sourceMappingURL=index.c36f364e.js.map
---
+++ server.js
@@ -0,0 +1,10 @@
+
+const math = require('./math')
+
+const resultAdd = math.add(9,5)
+const resultSub = math.subtract(20,5)
+
+console.log("Сумма", resultAdd )
+console.log("Разница", resultSub )
+
+
